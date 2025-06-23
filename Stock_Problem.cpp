682ca3afc6381555f8bab17a86c88a2af70bbbff
Pritam <pritamsaha102005@gmail.com>
2025-06-23 22:00:45 +0530

    New Program

--- Stock_Problem.cpp
+++ Stock_Problem.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 #include<vector>
+using namespace std;
 
 int main()
 {
     vector<int> price={7,4,1,6,5,2};
-    int maxPrice=
-
+    int MP=0,BB=price[0];
+    for(int i=1;i<price.size();i++)
+    {
+        if(price[i]>BB)
+        {
+            MP=max(MP,price[i]-BB);
+        }
+        BB=min(BB,price[i]);
+    }
+    cout<<"Maximum Profit is "<<MP<<endl;
     return 0;
 }
