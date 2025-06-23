e4967938ea0b8670848ad6cf807d023926ae7185
Pritam <pritamsaha102005@gmail.com>
2025-06-23 18:18:31 +0530

    Slightly Changed

--- MooresAlgo.cpp
+++ MooresAlgo.cpp
@@ -16,6 +16,27 @@ int main()
         else
             freq--;        
     }
-    cout<<"Majority element is "<<ans<<endl;
+    #include<iostream>
+using namespace std;
+#include<vector>
+
+int main()
+{
+    vector<int> nums={1,1,2,2,1};
+    int freq=0,ans=0;
+    int n=nums.size();
+    for(int i=0;i<n;i++)
+    {
+        if(freq==0)
+            ans=nums[i];
+        if(ans==nums[i])
+            freq++;
+        else
+            freq--;        
+    }
+    if (count > n / 2)
+        cout << "Majority element is " << ans << endl;
+    else
+        cout << "No majority element exists." << endl;
     return 0;
 }
