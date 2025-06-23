7d9f336d986e1a0b64f61215d7fdb51f81ca0fcd
Pritam <pritamsaha102005@gmail.com>
2025-06-23 21:50:27 +0530

    Changes committed

---
+++ Stock_Problem.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    vector<int> price={7,4,1,6,5,2};
+    int maxPrice=
+
+    return 0;
+}
--- majority_ele.cpp
+++ majority_ele.cpp
@@ -1,34 +1,34 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    vector<int> nums={2,2,1,1,1};
-    int value=-1;
-    for(int val: nums)
+    vector<int> nums = {2, 2, 1, 1, 1};
+    int value = -1;
+    for (int val : nums)
     {
-        int freq=0;
-        for(int ele: nums)
+        int freq = 0;
+        for (int ele : nums)
         {
-            if(val==ele)
+            if (val == ele)
             {
                 freq++;
             }
         }
-        if(freq>(nums.size()/2))
+        if (freq > (nums.size() / 2))
         {
-            value=val;
+            value = val;
             break;
         }
     }
-    if(value!=-1)
+    if (value != -1)
     {
-        cout<<"Majority element is "<<value<<endl;
+        cout << "Majority element is " << value << endl;
     }
     else
     {
-        cout<<"No majority element found!"<<endl;
+        cout << "No majority element found!" << endl;
     }
     return 0;
 }
