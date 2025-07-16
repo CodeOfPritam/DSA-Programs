4f1947d2d509ca6fc77fee3c80b421e4485552a1
Pritam <pritamsaha102005@gmail.com>
2025-07-17 00:24:56 +0530

    New Program

--- Aggressive_Cows.cpp
+++ Aggressive_Cows.cpp
@@ -1,38 +1,52 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int getDistance(vector<int> &arr,int n,int c)
+bool isPossible(vector<int> &arr, int n, int c, int minAllowedDist)
 {
-    int st=1,end=maxVal-minVal;
-    sort(arr.begin(),arr.end());
-    while(st<=end)
+    int cows = 1, lastStallPos = arr[0];
+    for (int i = 1; i < n; i++)
     {
-        int mid=st+(end-st)/2;
-        if(isPossible(arr,n,c,mid))
+        if ((arr[i] - lastStallPos) >= minAllowedDist)
         {
-            ans=mid;
-            st=mid+1;
+            cows++;
+            lastStallPos = arr[i];
+            if (cows >= c) // We can place all cows
+                return true;
+        }
+    }
+    return false;
+}
 
+int getDistance(vector<int> &arr, int n, int c)
+{
+    sort(arr.begin(), arr.end()); // Sort once
+    int st = 1;
+    int end = arr[n - 1] - arr[0]; // Max possible distance
+    int ans = -1;
+
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        if (isPossible(arr, n, c, mid))
+        {
+            ans = mid;
+            st = mid + 1; // Try for larger distance
         }
         else
         {
-            end=mid-1;
+            end = mid - 1; // Try smaller distance
         }
-
-
     }
+    return ans;
 }
 
 int main()
 {
-    int c=3,n=5;
-    vector<int> arr={1,2,8,4,9};
-    for(int i=0;i<5;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    int N = 5, C = 3;
+    vector<int> vec = {1, 2, 8, 4, 9};
+
+    cout << "Maximum value of minimum distance is " << getDistance(vec, N, C) << endl; // Expected output: 3
     return 0;
 }
