acb98e2025d46049a38db8bce77654d87a7737a0
Pritam <pritamsaha102005@gmail.com>
2025-07-20 16:40:40 +0530

    New Program

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,32 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
-
-void selection_sortArray(int *arr) {
-    int n = 6;  // Size of array (you can make this a parameter for more flexibility)
-
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+int main()
+{
+    vector<int> arr={4,1,5,2,3};
+    int n=arr.size();
+    for(int i=0;i<n-1;i++)
+    {
+        int minIndex=i;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]<arr[minIndex])
+            {
+                minIndex=j;
             }
         }
-
-        // Swap the current element with the minimum found
-        swap(arr[i], arr[minIndex]);
+        swap(arr[minIndex],arr[i]);
     }
-}
-
-int main() {
-    int arr[] = {9, 15, 5, 7, 3, 1};
-
-    selection_sortArray(arr);  // No need for '&'
-
-    for (int i = 0; i < 6; i++) {
-        cout << arr[i] << " ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
-
+    cout<<endl;
     return 0;
 }
