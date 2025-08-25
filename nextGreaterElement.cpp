e182892ce8f0e6c4f4bf01135b14a0b187493783
Pritam <pritamsaha102005@gmail.com>
2025-08-25 13:03:02 +0530

    New Program

--- nextGreaterElement.cpp
+++ nextGreaterElement.cpp
@@ -1,36 +1,33 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
-int main()
+
+vector<int> nextGreaterElement(vector<int> &arr)
 {
-    vector<int> arr={6,8,0,1,3};
-
-    //Next greater element
+    int n = arr.size();
     stack<int> s;
-    vector<int> ans(arr.size(),0);
-
-    for(int i=arr.size()-1;i>=0;i--)
+    vector<int> ans(n, 0);
+    for (int i = n - 1; i >= 0; i--)
     {
-        while(s.size()>0 && s.top()<=arr[i])
+        while (!s.empty() && s.top() <= arr[i])
         {
             s.pop();
         }
-        if(s.empty())
-        {
-            ans[i]=-1;
-        }
-        else
-        {
-            ans[i]=s.top();
-        }
+        ans[i] = s.empty() ? -1 : s.top();
         s.push(arr[i]);
     }
+    return ans;
+}
 
-    for(int val:ans)
+int main()
+{
+    vector<int> arr = {3, 1, 8, 5, 2};
+    vector<int> ans = nextGreaterElement(arr);
+    for (int val : ans)
     {
-        cout<<val<<" ";
+        cout << val << " ";
     }
-    cout<<endl;
+    cout << endl;
     return 0;
 }
