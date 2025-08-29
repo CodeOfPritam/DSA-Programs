4661b2e10bd9ed9bbfd2525a5c45b43562425213
Pritam <pritamsaha102005@gmail.com>
2025-08-29 22:47:43 +0530

    New Program

--- celebrityProblem.cpp
+++ celebrityProblem.cpp
@@ -3,45 +3,57 @@
 #include <stack>
 using namespace std;
 
-int getCelebrity(vector<vector<int>> &arr) {
+int getCelebrity(vector<vector<int>> &arr)
+{
     int n = arr.size();
     stack<int> s;
 
-    for (int i = 0; i < n; i++) {
+    // push all candidates
+    for (int i = 0; i < n; i++)
+    {
         s.push(i);
     }
 
-    while (s.size() > 1) {
+    // eliminate until one left
+    while (s.size() > 1)
+    {
         int i = s.top(); s.pop();
         int j = s.top(); s.pop();
 
-        if (arr[i][j] == 1) {
-            // i knows j -> i can't be celeb
+        if (arr[i][j] == 1)
+        {
+            // i knows j → i cannot be celebrity
             s.push(j);
-        } else {
-            // i doesn't know j -> j can't be celeb
+        }
+        else
+        {
+            // i does not know j → j cannot be celebrity
             s.push(i);
         }
     }
 
     int celeb = s.top();
 
-    // validation
-    for (int i = 0; i < n; i++) {
-        if (i != celeb) {
-            if (arr[i][celeb] == 0 || arr[celeb][i] == 1) {
-                return -1;
-            }
+    // validate celeb
+    for (int i = 0; i < n; i++)
+    {
+        if (i != celeb && (arr[i][celeb] == 0 || arr[celeb][i] == 1))
+        {
+            return -1;
         }
     }
-
     return celeb;
 }
 
-int main() {
-    vector<vector<int>> arr = {{0,1,0},{1,0,0},{0,1,0}};
+int main()
+{
+    vector<vector<int>> arr = {{0,1,0}, {0,0,0}, {0,1,0}};
     int ans = getCelebrity(arr);
-    if(ans == -1) cout << "No Celebrity\n";
-    else cout << "Celebrity is: " << ans << endl;
+
+    if (ans != -1)
+        cout << "Celebrity is : " << ans << endl;
+    else
+        cout << "No celebrity" << endl;
+
     return 0;
 }
