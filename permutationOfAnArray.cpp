854768d765fc0aeab32d9a6ed44a5c3218fb0135
Pritam <pritamsaha102005@gmail.com>
2025-09-02 16:42:58 +0530

    New Program

---
+++ permutationOfAnArray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void getPerms(vector<int> &nums, int idx, vector<vector<int>> &ans)
+{
+    if (idx == nums.size())
+    {
+        ans.push_back(nums);
+        return;
+    }
+
+    for (int i = idx; i < nums.size(); i++)
+    {
+        swap(nums[idx], nums[i]);
+        getPerms(nums, idx + 1, ans);
+        swap(nums[idx], nums[i]); // backtrack
+    }
+}
+
+vector<vector<int>> permute(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    getPerms(nums, 0, ans);
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3};
+    vector<vector<int>> ans = permute(arr);
+
+    // Print all permutations
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
