ac715436dbbeae30094ce036a388be5bcfe54747
Pritam <pritamsaha102005@gmail.com>
2025-09-24 16:39:33 +0530

    New Program

--- rat_in_a_maze.cpp
+++ rat_in_a_maze.cpp
@@ -2,41 +2,43 @@
 #include<vector>
 using namespace std;
 
-void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans, vector<vector<bool>> &vis)
+void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans)
 {
-    int n=mat.size();
+    int n = mat.size();
 
-    if(r<0 || c<0 || r>=n || c>=n || mat[r][c]==0 || vis[r][c]==true)
-    {
+    // Out of bounds / blocked / already visited
+    if(r<0 || c<0 || r>=n || c>=n || mat[r][c]==0 || mat[r][c]==-1)
         return;
-    }
 
-    if(r==n-1 && c==n-1) //ans
+    // Reached destination
+    if(r==n-1 && c==n-1)
     {
         ans.push_back(path);
         return;
     }
 
-    vis[r][c] = true;  //marked as visited
+    // Mark visited
+    mat[r][c] = -1;
 
-    helper(mat, r+1, c, path+"D", ans, vis); //down
-    helper(mat, r-1, c, path+"U", ans, vis); //up
-    helper(mat, r, c-1, path+"L", ans, vis); //left
-    helper(mat, r, c+1, path+"R", ans, vis); //right
+    // Explore all 4 directions
+    helper(mat, r+1, c, path+"D", ans); // Down
+    helper(mat, r-1, c, path+"U", ans); // Up
+    helper(mat, r, c-1, path+"L", ans); // Left
+    helper(mat, r, c+1, path+"R", ans); // Right
 
-    vis[r][c] = false; //marked as not visited
+    // Backtrack
+    mat[r][c] = 1;
 }
 
 vector<string> findPath(vector<vector<int>> &mat)
 {
-    int n=mat.size();
+    int n = mat.size();
 
     vector<string> ans;
     string path="";
-    vector<vector<bool>> vis(n, vector<bool>(n, false));
+    // vector<vector<bool>> vis(n, vector<bool>(n, false));
 
-    if(mat[0][0] == 1)  
-        helper(mat, 0, 0, path, ans, vis);
+    helper(mat, 0, 0, path, ans);
 
     return ans; 
 }
