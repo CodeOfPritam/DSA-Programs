a7eaf23f31e1d0db28cfbd74abdef9bb671d37f2
Pritam <pritamsaha102005@gmail.com>
2025-10-23 18:34:03 +0530

    New Program

---
+++ BST_IS_and_IP.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node *rightMostInLeftSubtree(Node *root) {
+    Node *ans = NULL;
+    while (root != NULL) {
+        ans = root;
+        root = root->right;
+    }
+    return ans;
+}
+
+Node *leftMostInRightSubtree(Node *root) {
+    Node *ans = NULL;
+    while (root != NULL) {
+        ans = root;
+        root = root->left;
+    }
+    return ans;
+}
+
+vector<int> getPredSucc(Node *root, int key) {
+    Node *curr = root;
+    Node *pred = NULL;
+    Node *succ = NULL;
+
+    while (curr != NULL) {
+        if (key < curr->data) {
+            succ = curr;
+            curr = curr->left;
+        } 
+        else if (key > curr->data) {
+            pred = curr;
+            curr = curr->right;
+        } 
+        else {
+            if (curr->left != NULL)
+                pred = rightMostInLeftSubtree(curr->left);
+
+            if (curr->right != NULL)
+                succ = leftMostInRightSubtree(curr->right);
+
+            break;
+        }
+    }
+
+    int predVal = (pred) ? pred->data : -1;
+    int succVal = (succ) ? succ->data : -1;
+    return {predVal, succVal};
+}
+
+int main() {
+    Node *root = new Node(6);
+    root->left = new Node(4);
+    root->right = new Node(8);
+    root->left->left = new Node(1);
+    root->left->right = new Node(5);
+    root->right->left = new Node(7);
+    root->right->right = new Node(9);
+
+    int key = 7;
+    vector<int> ans = getPredSucc(root, key);
+
+    cout << "Inorder predecessor: " << ans[0] << endl;
+    cout << "Inorder successor: " << ans[1] << endl;
+
+    return 0;
+}
